c10e7743bfdb9238da3f1aaae588b73a71f4ca93
Ana-Gabriel <ana.gabriel2012@gmail.com>
2016-11-28 00:52:00 -0500

    added accept friend

--- src/accept_friend.sql
+++ src/accept_friend.sql
@@ -14,7 +14,7 @@ BEGIN
  -- check that the user isn't in the group already
  SELECT count(user_id) AS count INTO X FROM user_frien_relation
    WHERE (user_id = uid AND friend_id = fid) OR (user_id = fid AND friend_id = uid);
- -- Check they are nonzero
+ -- check they are nonzero
  IF fid = 0 or uid = 0 THEN
   SET success = 0;
  ELSEIF X != 0 THEN
