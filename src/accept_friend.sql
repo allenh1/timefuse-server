b4c822b980b7fdbd66a9ee35b23336d69f8c5ff8
Ana-Gabriel <ana.gabriel2012@gmail.com>
2016-11-28 18:02:58 -0500

    adding/accepting/deleting friends works

--- src/accept_friend.sql
+++ src/accept_friend.sql
@@ -12,18 +12,16 @@ BEGIN
  -- Find the friend id
  SELECT user_id AS count INTO fid FROM users WHERE user_name = friend;
  -- check that the user isn't in the group already
- SELECT count(user_id) AS count INTO X FROM user_friend_relation
+ SELECT relation_id AS count INTO X FROM user_friend_relation
    WHERE (user_id = uid AND friend_id = fid)
    OR (user_id = fid AND friend_id = uid);
  -- check they are nonzero
- IF fid = 0 or uid = 0 THEN
-  SET success = 0;
- ELSEIF X != 0 THEN
+ IF fid = 0 or uid = 0 or X = 0 THEN
   SET success = 0;
  ELSE
   -- add an element to the user friend relation.
   UPDATE user_friend_relation SET accepted=1
-  WHERE user_id = uid AND friend_id = fid;
+  WHERE relation_id = X;
   SET success = 1;
  END IF;
 END$$
