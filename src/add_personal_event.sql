df8f3732ccdeb35bab9a33644ef90ba3566d156f
Hunter L. Allen <allen286@purdue.edu>
2016-11-29 16:21:49 -0500

    Added the timezone settings to the worker node and stored procedure.

--- src/add_personal_event.sql
+++ src/add_personal_event.sql
@@ -7,6 +7,7 @@ CREATE PROCEDURE AddPersonalEvent(
  IN startTime TIME,
  IN eventDuration TIME,
  IN eventLocation VARCHAR(512),
+ IN eventOffset INT,
  IN eventName VARCHAR(512),
  IN immutable BOOLEAN,
  OUT success BOOLEAN)
@@ -24,13 +25,13 @@ BEGIN
   IF !immutable THEN
    -- interpret parameters differently.
    INSERT INTO schedule_item(date, start_time, duration, location,
-    event_name, schedule_id, immutable, deadline_date, deadline_time)
+    timezone_offset, event_name, schedule_id, immutable, deadline_date, deadline_time)
 	VALUES(eventDate, SEC_TO_TIME(TIME_TO_SEC(startTime) - TIME_TO_SEC(eventDuration)),
-	eventDuration, eventLocation, eventName, sid, immutable, eventDate, startTime);
+	eventDuration, eventLocation, eventOffset, eventName, sid, immutable, eventDate, startTime);
   ELSE
    INSERT INTO schedule_item(date, start_time, duration, location,
-    event_name, schedule_id, immutable) VALUES(eventDate, startTime,
-	eventDuration, eventLocation, eventName, sid, immutable);
+    timezone_offset, event_name, schedule_id, immutable) VALUES(eventDate, startTime,
+	eventDuration, eventLocation, eventOffset, eventName, sid, immutable);
   END IF;
   SET success = 1;
  END IF;
